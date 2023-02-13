11331049dedbb74364d879c9d319ebdba9de417b
benparkur <gary.monroe@kippstudent.org>
2023-02-13 17:43:01 +0000

    Finished todo 6

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -79,11 +79,18 @@ var init = function (window) {
             }
             
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
-            
-
+            if(circle.x < 0){
+                circle.x = canvas.width;
+            }
+            if(circle.y > canvas.height){
+                circle.y = 0;
+            }
+            if(circle.y < 0){
+                circle.y = canvas.height;
+            }
 
             // YOUR TODO 6 CODE ENDS HERE //////////////////////////
-        }
+        }   
         
         /////////////////////////////////////////////////////////////
         // --- NO CODE BELOW HERE  --- DO NOT REMOVE THIS CODE --- //
